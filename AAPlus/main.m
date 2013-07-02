21661b07470830d9d326a705de86e5cb7bde7564
ragarmakhis <ragarmakhis@gmail.com>
2013-07-02 11:15:50 +0400

    Initial Commit

---
+++ AAPlus/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AAPlus
+//
+//  Created by Logger 1 on 02.07.13.
+//  Copyright (c) 2013 ragarmakhis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
